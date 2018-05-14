d094ae7c7ffdb06d978a026c66d8d1b3da0b36c5
johasago <jo@gowithjo.online>
2018-05-14 19:47:19 +0100

    first practice at creating front page

--- view/pages/main_page.php
+++ view/pages/main_page.php
@@ -1,17 +1,19 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php require_once('../config.php');
+require_once( ROOT_PATH . '/pages/includes/head_section.php');
+require_once ('../publicfunctions.php');
+$posts = getPublishedPosts(); ?>
+
+  <title>Sexy Salads</title>
+</head>
+  <body>
+
+<?php include ( ROOT_PATH . '/pages/includes/navbar.php') ?>      
+
+<?php include ( ROOT_PATH . '/pages/includes/carousel.php') ?>
+
+<?php include ( ROOT_PATH . '/pages/includes/sidebar.php') ?>
+
+      
+      
+    
+  <?php include ( ROOT_PATH . '/pages/includes/footer.php') ?>
